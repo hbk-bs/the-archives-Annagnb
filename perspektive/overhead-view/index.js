ad24a09b131f9ffbc7ea1741a6faf65dc5048046
Annagnb <a.gonsberg@hbk-bs.de>
2025-03-03 14:01:46 +0100

    perspektive-speichern2

--- perspektive/overhead-view/index.js
+++ perspektive/overhead-view/index.js
@@ -1,9 +1,52 @@
+const x = (n) => width * n;
+const y = (n) => height * n;
+const s = (n) => (height > width ? height * n : width * n);
 
+function setup() {
+	createCanvas(300, 200);
 
-function setup(){
-  const canvas = createCanvas(100,100);
-  canvas.parent("sketch");
-  background("black");
+	colorMode(HSB);
+	background(178, 66, 82);
+
+	drawFish(width / 2, height / 2, 100);
 }
 
-function draw() {}
+function drawFish(x, y, size) {
+	push();
+	translate(x, y); 
+
+	
+	fill(17, 52, 100);
+	noStroke();
+	triangle(
+		-size * 0.5,
+		0, 
+		-size * 1.0,
+		-size * 0.15, 
+		-size * 1.1,
+		size * 0.1 
+	);
+
+	
+	noStroke();
+	ellipse(0, 0, size * 1.2, size * 0.6); 
+
+	
+	fill(0, 0, 100);
+	ellipse(size * 0.25, -size * 0.15, size * 0.15, size * 0.1); 
+	ellipse(size * 0.25, size * 0.15, size * 0.15, size * 0.1); 
+
+	
+	fill(0, 0, 0);
+	circle(size * 0.27, -size * 0.15, size * 0.04 * 2);
+	circle(size * 0.27, size * 0.15, size * 0.04 * 2);
+
+	
+	fill(6, 12, 100);
+	ellipse(-size * 0.3, -size * 0.3, size * 0.3, size * 0.15);
+	ellipse(-size * 0.3, size * 0.3, size * 0.3, size * 0.15);
+
+	pop();
+}
+
+
--- perspektive/wide-angle/index.js
+++ perspektive/wide-angle/index.js
@@ -1,9 +1,38 @@
+const x = (n) => width * n;
+const y = (n) => height * n;
+const s = (n) => (height > width ? height * n : width * n);
 
+function setup() {
+	createCanvas(700, 400);
+	colorMode(HSB);
+	background(0, 0, 50);
 
-function setup(){
-  const canvas = createCanvas(100,100);
-  canvas.parent("sketch");
-  background("black");
+	stroke(30, 60, 60);
+	fill(40, 30, 91);
+	rect(x(0), y(0.9), x(5), s(5));
+	fill("tan");
+	rect(x(0), y(0.55), x(1), s(0.2));
+
+	stroke(195, 40, 95);
+	fill(177, 70, 85);
+	rect(x(0.14), y(0.1), s(0.43), s(0.43)); 
+	quad(x(0.57), y(0.1), x(0.67), y(0.0125), x(0.67), y(0.7), x(0.57), y(0.85)); 
+	quad(x(0.57), y(0.1), x(0.67), y(0.0125), x(0.23), y(0.0125), x(0.14), y(0.1));
+
+	stroke(270, 50, 80);
+	fill(240, 15, 100);
+	triangle(x(0.37), y(0.275), x(0.47), y(0.375), x(0.47), y(0.15));
+	ellipse(x(0.3), y(0.275), s(0.18), s(0.16));
+
+	stroke(6, 60, 100);
+	fill(17, 40, 100);
+	ellipse(x(0.21), y(0.27), s(0.025), s(0.025));
+	ellipse(x(0.21), y(0.295), s(0.025), s(0.025));
+
+	stroke(0, 0, 0);
+	fill(0, 0, 100);
+	ellipse(x(0.27), y(0.24), s(0.06), s(0.04));
+	fill(0, 0, 0);
+	strokeWeight(22);
+	point(x(0.27), y(0.235));
 }
-
-function draw() {}
