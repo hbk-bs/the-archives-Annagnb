af60615f7228bdbdc5e6d2e5ddbc158f251a9328
Annagnb <a.gonsberg@hbk-bs.de>
2025-03-10 18:05:54 +0100

    test

--- blinkenlichten/index.js
+++ blinkenlichten/index.js
@@ -19,47 +19,24 @@ void loop() {
   output_value = digitalRead(OUT_PIN);
   if (output_value) {
     Serial.println("Objekt erkannt!");
-    // fill_solid(leds, NUM_LEDS, CRGB(255,0,0));
-    //     FastLED.show();
+    
   for (int i = 0; i < 256; i++) {
          fill_solid(leds, NUM_LEDS, CRGB(i, i, i));
-    // leds[current_led % NUM_LEDS] = CRGB(i, i, i);
+   
     FastLED.show();
     delay(10);
   }
     motion_detected = true;
     delay(10);
   } else {
-  //         for (int i = 255; i >= 0; i--) {
-  //         fill_solid(leds, NUM_LEDS, CRGB(i, i, i));
-  //   //leds[current_led % NUM_LEDS] = CRGB(i, i, i);
-  //   FastLED.show();
-  //   delay(10);
-  // }
+  
          fill_solid(leds, NUM_LEDS, CRGB::Black);
   FastLED.show();
     delay(100);
     Serial.println("Kein Objekt!");
   }
-  // if (motion_detected) {
-  //   Serial.println("Warten!");
-  //   delay(20);
-  //   Serial.println("Bereit...");
-  //   motion_detected = false;
-  // }
+  
 }
-
-
-
-
-
-
-
-
-
-
-
-Nachricht an Andrea Kost Avila
 
 
 
